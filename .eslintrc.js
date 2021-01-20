2a9fffb29cd3fd5c31ca7a132dcbbf261bbae6f8
hirai <hirai@welldone.co.jp>
2021-01-20 10:29:11 +0900

    :adhesive_bandage: fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["src/pages/*.tsx"],
+      files: ["src/pages/**/*.tsx"],
       rules: {
         "import/no-default-export": "off",
       },
