235e2423f3a7241f190d98ee70f1608b0a77f948
magako <qutianh@gmail.com>
2021-01-21 01:50:59 +0900

    ダークモードの導入（next-themeの追加）

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,21 @@
 import Head from "next/head";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { Layout } from "src/components/layout";
 
 const About = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    return setMounted(true);
+  }, []);
+
+  const handleThemeChange = (value: string) => {
+    setTheme(value);
+  };
+  if (!mounted) return null;
+
   return (
     <Layout>
       <Head>
@@ -9,6 +23,22 @@ const About = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h2>About</h2>
+      <p>the current theme is: {theme}</p>
+      <select
+        onBlur={(selectedOptinon) => {
+          return handleThemeChange(selectedOptinon.target.value);
+        }}
+        onChange={(selectedOptinon) => {
+          return handleThemeChange(selectedOptinon.target.value);
+        }}
+      >
+        <option selected={theme === "dark"} value="dark">
+          Dark
+        </option>
+        <option selected={theme === "light"} value="light">
+          Light
+        </option>
+      </select>
     </Layout>
   );
 };
