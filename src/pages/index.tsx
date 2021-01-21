57ceb8f734726f3a2d5bce1bb4d5f2675403e74b
reeve0930 <reeve0930@gmail.com>
2021-01-21 23:41:03 +0900

    ✨ Add next-themes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,9 @@ const Home = () => {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h2>Home</h2>
+      <h2 className="text-gray-800 dark:text-white">Home</h2>
       <button
+        className="text-gray-800 dark:text-white"
         onClick={() => {
           window.alert("Hello, World!");
         }}
