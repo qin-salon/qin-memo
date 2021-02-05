365e119b8aa2ee62999cb323c107627918dc8991
skoto <0120koto@gmail.com>
2021-02-05 20:22:42 +0900

    🔧 Add swr config.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,25 @@ import "src/styles/global.css";
 
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { SWRConfig } from "swr";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   require("libs/mocks");
 }
 
+const fetcher = async (input: RequestInfo, init?: RequestInit) => {
+  return fetch(input, init).then((res) => {
+    return res.json();
+  });
+};
+
 const App = (props: AppProps) => {
   return (
-    <ThemeProvider attribute="class">
-      <props.Component {...props.pageProps} />
-    </ThemeProvider>
+    <SWRConfig value={{ fetcher }}>
+      <ThemeProvider attribute="class">
+        <props.Component {...props.pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   );
 };
 
