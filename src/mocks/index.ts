704b43a15040ac094d05f1ee0879f6ddcf4d867b
skoto <0120koto@gmail.com>
2021-03-11 14:40:06 +0900

    ✨ MSW init setting.

---
+++ src/mocks/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+if (typeof window === "undefined") {
+  const { server } = require("./server");
+  server.listen();
+} else {
+  const { worker } = require("./browser");
+  worker.start();
+}
+
+export {};
