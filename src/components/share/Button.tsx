79ec4c273daadf6e5b549fc0bd2b510856633646
takasaki376 <75880059+takasaki376@users.noreply.github.com>
2021-03-14 15:04:16 +0900

    :ambulance:Buttonコンポーネントのprops型定義変更

--- src/components/share/Button.tsx
+++ src/components/share/Button.tsx
@@ -1,100 +1,90 @@
 import clsx from "clsx";
 import Link from "next/link";
-import type { ReactNode, VFC } from "react";
+import type { DOMAttributes, ReactNode, VFC } from "react";
 
-interface Props {
+type CommonType = {
   id: string;
   color?: "blue" | "red" | "black" | "white" | "gray" | "orange";
   textColor?: "black" | "red" | "blue";
   disabled?: boolean;
   children?: ReactNode;
-  type?: "button" | "link";
-  linkProps?: string;
   className?: string;
   StartIcon?: VFC;
   EndIcon?: VFC;
   size?: "large" | "small";
-  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
-}
+};
+type ButtonType = CommonType & {
+  type: "button";
+  onClick: DOMAttributes<HTMLButtonElement>["onClick"];
+};
 
-const Button: VFC<Props> = (props) => {
-  // props受け取り、デフォルト値設定
-  const {
-    id,
-    color = "bule",
-    textColor = "black",
-    disabled = false,
-    className = "",
-    children,
-    type = "button",
-    linkProps = "",
-    StartIcon,
-    EndIcon,
-    size = "small",
-    onClick,
-  } = props;
+type LinkType = CommonType & {
+  type: "link";
+  linkProps: string;
+};
+
+const Button: VFC<ButtonType | LinkType> = (props) => {
+  // props の型がbutton型かlink型かを判断する関数を定義する。
+  // props にonClickを持っている場合は true
+  const isButton = (props: ButtonType | LinkType): props is ButtonType => {
+    return "onClick" in props;
+  };
 
   // ボタンのCSS
   const classes = clsx([
     " my-4 mx-auto rounded-full focus:outline-none flex flex-row justify-center",
     {
-      "text-white bg-blue-500 hover:bg-blue-600": color === "blue" && disabled === false,
-      "text-white bg-red-500 hover:bg-red-600": color === "red" && disabled === false,
-      "text-white bg-yellow-500 hover:bg-yellow-600": color === "orange" && disabled === false,
-      "text-black bg-gray-300 hover:bg-gray-400": color === "gray" && disabled === false,
-      "text-white bg-black hover:bg-gray-500": color === "black" && disabled === false,
-      " bg-white hover:bg-gray-300": color === "white" && disabled === false,
-      "text-black": color === "white" && disabled === false && textColor === "black",
-      "text-red-500": color === "white" && disabled === false && textColor === "red",
-      "text-blue-500": color === "white" && disabled === false && textColor === "blue",
-      "text-gray-400 bg-gray-300": disabled,
-      "py-4 px-8": size === "large",
-      "py-2 px-4": size === "small",
+      "text-white bg-blue-500 hover:bg-blue-600": props.color === "blue" && props.disabled === false,
+      "text-white bg-red-500 hover:bg-red-600": props.color === "red" && props.disabled === false,
+      "text-white bg-yellow-500 hover:bg-yellow-600": props.color === "orange" && props.disabled === false,
+      "text-black bg-gray-300 hover:bg-gray-400": props.color === "gray" && props.disabled === false,
+      "text-white bg-black hover:bg-gray-500": props.color === "black" && props.disabled === false,
+      "bg-white hover:bg-gray-300": props.color === "white" && props.disabled === false,
+      "text-black": props.color === "white" && props.disabled === false && props.textColor === "black",
+      "text-red-500": props.color === "white" && props.disabled === false && props.textColor === "red",
+      "text-blue-500": props.color === "white" && props.disabled === false && props.textColor === "blue",
+      "text-gray-400 bg-gray-300": props.disabled,
+      "py-4 px-8": props.size === "large",
+      "py-2 px-4": props.size === "small",
     },
-    className,
+    props.className,
   ]);
   const iconClasses = clsx([
     "my-auto",
     {
-      "mx-3": size === "large",
-      "mx-2": size === "small",
+      "mx-3": props.size === "large",
+      "mx-2": props.size === "small",
     },
   ]);
 
-  const handleClick = () => {
-    if (disabled) return null;
-    return onClick;
-  };
   return (
     <div className="mx-auto">
-      {type === "button" && (
-        <span data-testid={id} className={classes} onClick={handleClick}>
-          {StartIcon && (
+      {isButton(props) ? (
+        <span data-testid={props.id} className={classes} onClick={props.onClick}>
+          {props.StartIcon && (
             <div className={iconClasses}>
-              <StartIcon />
+              <props.StartIcon />
             </div>
           )}
-          <strong>{children}</strong>
-          {EndIcon && (
+          <strong>{props.children}</strong>
+          {props.EndIcon && (
             <div className={iconClasses}>
-              <EndIcon />
+              <props.EndIcon />
             </div>
           )}
         </span>
-      )}
-
-      {type === "link" && disabled === false && (
-        <Link href={linkProps}>
-          <span data-testid={id} className={classes} onClick={handleClick}>
-            {StartIcon && (
+      ) : (
+        <Link href={props.linkProps}>
+          <span data-testid={props.id} className={classes}>
+            {props.StartIcon && (
               <div className={iconClasses}>
-                <StartIcon />
+                <props.StartIcon />
               </div>
             )}
-            <strong>{children}</strong>
-            {EndIcon && (
+            <strong>{props.children}</strong>
+            {props.EndIcon && (
               <div className={iconClasses}>
-                <EndIcon />
+                <props.EndIcon />
               </div>
             )}
           </span>
@@ -103,5 +93,13 @@ const Button: VFC<Props> = (props) => {
     </div>
   );
 };
-
+// Propsのデフォルト値
+Button.defaultProps = {
+  color: "blue",
+  textColor: "black",
+  disabled: false,
+  className: "",
+  type: "button",
+  size: "small",
+};
 export default Button;
