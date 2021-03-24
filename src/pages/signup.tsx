f0c1de0f755bbeb364c8ce40f4d184728a00e05d
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2021-03-24 20:56:22 +0900

    ページ名を付けて再アップ

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -6,10 +6,14 @@ const SignUp: NextPage = () => {
   return (
     <div className="w-96 min-h-screen bg-white">
       <div className="flex flex-col justify-center text-center">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path
+            fillRule="evenodd"
+            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+            clipRule="evenodd"
+          />
         </svg>
-        <LogoIcon size="large" />
+        <LogoIcon size="larges" />
         <div className="w-80 h-14 bg-white rounded-full relative">
           <div className="flex justify-around">
             <GoogleIcon size="large" />
