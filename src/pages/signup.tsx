ced965d93ef6d28bb6ee0abb3e341d29dd6ffc68
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2021-03-25 20:24:48 +0900

    お手本を複写

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,29 +1,26 @@
 import type { NextPage } from "next";
+import { AppleIcon } from "src/components/icon/AppleIcon";
+import { ChevronLeft } from "src/components/icon/ChevronLeft";
 import { GoogleIcon } from "src/components/icon/GoogleIcon";
 import { LogoIcon } from "src/components/icon/LogoIcon";
+import { Button } from "src/components/shared/Button";
 
 const SignUp: NextPage = () => {
   return (
-    <div className="w-96 min-h-screen bg-gray-50">
-      <div className="flex flex-col justify-center">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fillRule="evenodd"
-            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <div>
-          <div className="flex justify-center">
-            <LogoIcon className="block" size="large" />
-          </div>
-          <div className="w-80 h-14 bg-white rounded-full relative">
-            <div className="flex justify-around">
-              <GoogleIcon size="large" />
-              <p>Googleでアカウントを作成</p>
-            </div>
-          </div>
+    <div className="bg-gray-50 w-screen h-screen ">
+      <Button button id="backButton" bgColor="transparent" className="absolute left-2">
+        <ChevronLeft />
+      </Button>
+      <div className="h-screen flex justify-center flex-col">
+        <div className="flex justify-center mb-16">
+          <LogoIcon />
         </div>
+        <Button button id="googleButton" bgColor="white" startIcon={<GoogleIcon />} className="w-80" size="large">
+          Googleでアカウントを作成
+        </Button>
+        <Button button id="appleButton" bgColor="black" startIcon={<AppleIcon />} className="w-80" size="large">
+          Appleでアカウントを作成
+        </Button>
       </div>
     </div>
   );
