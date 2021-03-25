41a085b86fbbd064a77e977bf086098f425d7967
yuji-bvb <yuji.bvb@gmail.com>
2021-03-25 11:03:30 +0900

    💄 signup追加

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,31 @@
 import type { NextPage } from "next";
+import { AppleIcon } from "src/components/icon/AppleIcon";
+import { ChevronLeft } from "src/components/icon/ChevronLeft";
+import { GoogleIcon } from "src/components/icon/GoogleIcon";
+import { LogoIcon } from "src/components/icon/LogoIcon";
+import { Button } from "src/components/shared/Button";
 
 const SignUp: NextPage = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <div className="bg-gray-200 w-screen h-screen">
+        <Button button id="backButton" bgColor="transparent" className="absolute left-2">
+          <ChevronLeft />
+        </Button>
+        <div className="h-screen flex flex-col justify-center">
+          <div className="flex justify-center mb-16">
+            <LogoIcon />
+          </div>
+          <Button button id="googleButton" bgColor="white" startIcon={<GoogleIcon />} size="large" className="w-80">
+            Googleでアカウントを作成
+          </Button>
+          <Button button id="appleButton" bgColor="black" startIcon={<AppleIcon />} size="large" className="w-80">
+            Appleでアカウントを作成
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
