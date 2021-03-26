9b1a1e558928cb7e6cce03c2883d328b93821355
hirata <>
2021-03-27 01:10:41 +0900

    :lipstick: padding追加

--- src/pages/settings/account.tsx
+++ src/pages/settings/account.tsx
@@ -7,10 +7,10 @@ import { Button } from "src/components/shared/Button";
 
 const SettingsAccount: NextPage = () => {
   return (
-    <div className="bg-white h-screen flex justify-center text-black">
+    <div className="bg-white h-screen flex justify-center px-5 text-black">
       <div className="w-80">
         <div className="relative flex justify-center mb-6 py-3">
-          <Button button id="backButton" bgColor="transparent" className="absolute left-0 top-0 mx-0 my-0 px-0 py-3.5">
+          <Button button bgColor="transparent" className="absolute left-0 top-0 mx-0 my-0 px-0 py-3.5">
             <ChevronLeft />
           </Button>
           <QinAccount />
@@ -21,16 +21,16 @@ const SettingsAccount: NextPage = () => {
             <GoogleIcon />
             <p className="ml-3 font-bold">Google</p>
           </div>
-          <Button button id="" bgColor="gray" className="my-0 mx-0 text-xs">
+          <Button button bgColor="gray" className="my-0 mx-0 text-xs">
             解除する
           </Button>
         </div>
         <div className="flex justify-between items-center mb-3">
           <div className="flex items-center">
-            <AppleIcon className="fill-current" />
+            <AppleIcon className="fill-current" iconColor="black" />
             <p className="ml-3 font-bold">Apple</p>
           </div>
-          <Button button id="" bgColor="blue" className="my-0 mx-0 text-xs">
+          <Button button bgColor="blue" className="my-0 mx-0 text-xs">
             連携する
           </Button>
         </div>
