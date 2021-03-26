600d6e3490b3ea1fd1197d38a55847646c24da0a
takasaki376 <75880059+takasaki376@users.noreply.github.com>
2021-03-26 21:53:12 +0900

    🚩 fetchに型定義を指定できるように共通処理定義

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,30 @@
 export const userUrl = "/users";
 
-// 検索
-export const getApi = (url: string) => {
-  fetch(url).then((res) => {
-    return res.json();
+// Tはレスポンスのjsonの型を指定する
+const wrap = <T>(note: Promise<Response>): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    note
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((json) => {
+              // jsonが取得できた場合だけresolve
+              resolve(json);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } else {
+          reject(response);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
+};
+// fetchに型定義を指定できる。
+export const fetcher = <T = any>(input: RequestInfo, init?: RequestInit): Promise<T> => {
+  return wrap<T>(fetch(input, init));
 };
