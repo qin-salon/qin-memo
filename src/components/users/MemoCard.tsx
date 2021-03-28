ca821441854cda9ae69af3ec448b34446ad42290
takasaki376 <75880059+takasaki376@users.noreply.github.com>
2021-03-28 18:48:17 +0900

    📱 レスポンシブデザイン対応

--- src/components/users/MemoCard.tsx
+++ src/components/users/MemoCard.tsx
@@ -12,18 +12,17 @@ export const MemoCard: VFC<Props> = (props) => {
   const title = String(excTitle ? excTitle[0] : "");
   return (
     <div className="bg-gray-200 my-4 rounded-3xl w-full h-32 mx-auto p-2">
-      <div className="inline-block mt-2 truncate">
-        <strong>
+      <strong>
+        <div className="h-6 mt-2 truncate">
           <span>{title}</span>
-        </strong>
-      </div>
-
+        </div>
+      </strong>
       <div className="my-2 truncate">
         <span>{props.note.excerpt.replace(title, "")}</span>
       </div>
       <div className="flex flex-row justify-between items-end ">
         <div className="pb-2">
-          <span>2021/3/25</span>
+          <span>{props.note.date}</span>
         </div>
         <div>
           {props.note.public ? (
