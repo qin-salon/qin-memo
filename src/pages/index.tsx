f6ef356026e2377bb0e0c785da487eeb10e3e699
skoto <0120koto@gmail.com>
2021-03-30 22:33:17 +0900

    🎨 開発しやすいように完成し終えたページにチェックマークを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,14 @@ import { Layout } from "src/components/layout";
 
 const PAGES = [
   { href: "/signin", file: "/signin.tsx", label: "ログインページ", isDone: true },
-  { href: "/signup", file: "/signup.tsx", label: "新規登録ページ", isDone: false },
-  { href: "/registration", file: "/registration.tsx", label: "初回プロフィール登録ページ", isDone: false },
+  { href: "/signup", file: "/signup.tsx", label: "新規登録ページ", isDone: true },
+  { href: "/registration", file: "/registration.tsx", label: "初回プロフィール登録ページ", isDone: true },
   { href: "/users/foo", file: "/users/[userId].tsx", label: "ユーザーページ", isDone: false },
   { href: "/search", file: "/search.tsx", label: "メモ検索ページ", isDone: false },
   { href: "/notes/foo", file: "/notes/[noteId].tsx", label: "メモページ", isDone: false },
   { href: "/settings", file: "/settings.tsx", label: "設定一覧ページ", isDone: false },
-  { href: "/settings/profile", file: "/settings/profile.tsx", label: "プロフィール変更ページ", isDone: false },
-  { href: "/settings/account", file: "/settings/account.tsx", label: "SNS連携ページ", isDone: false },
+  { href: "/settings/profile", file: "/settings/profile.tsx", label: "プロフィール変更ページ", isDone: true },
+  { href: "/settings/account", file: "/settings/account.tsx", label: "SNS連携ページ", isDone: true },
   // { href: "/settings/notification", file: "/settings/notification.tsx", label: "通知設定ページ", isDone: false },
   // { href: "/terms", file: "/terms", label: "利用規約ページ", isDone: false },
   // { href: "/privacy", file: "/privacy", label: "プライバシーポリシーページ", isDone: false },
