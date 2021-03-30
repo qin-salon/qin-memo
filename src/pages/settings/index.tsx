d42339e6ff2d5665572b822797a22d81ea5be3f3
kondo tomonori <tomonorik1001@gmail.com>
2021-03-30 21:26:22 +0900

    :lipstick: 戻るボタンの追加

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link";
 import type { NextPage } from "next";
 import { Button } from "src/components/shared/Button";
 import { ProfileSetting } from "src/components/shared/ProfileSetting";
+import { ChevronLeft } from "src/components/icon/ChevronLeft";
 
 const Settings: NextPage = () => {
   return (
     <div className="max-w-screen-sm mx-auto p-4">
-      <p className="my-4 text-center text-xl font-bold">プロフィール設定</p>
+      <p className="relative my-4 flex items-center">
+        <Link href="/">
+          <a className="absolute left-1">
+            <ChevronLeft />
+          </a>
+        </Link>
+        <span className="block text-center w-full text-xl font-bold">プロフィール設定</span>
+      </p>
       <ProfileSetting />
       <Button button className="w-full">
         保存する
