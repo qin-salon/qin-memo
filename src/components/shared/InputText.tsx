c406a98d1c03fbb2bdb34c9e62d8787cd134098c
takasaki376 <75880059+takasaki376@users.noreply.github.com>
2021-04-03 14:02:53 +0900

    🩹 スタイルの変数名変更

--- src/components/shared/InputText.tsx
+++ src/components/shared/InputText.tsx
@@ -29,7 +29,7 @@ const isTextarea = (props: TextareaType | InputType): props is TextareaType => {
 
 export const InputText: VFC<TextareaType | InputType> = (props) => {
   // テキストボックスにフォーカスがある場合にボーダーラインの表示を切り替える
-  const divStyle = clsx([
+  const divClasses = clsx([
     "flex text-center my-auto outline-none transition-colors",
     {
       "bg-gray-100": props.bgColor === "gray",
@@ -39,14 +39,14 @@ export const InputText: VFC<TextareaType | InputType> = (props) => {
     },
     props.className,
   ]);
-  const inputStyle = clsx([
+  const inputClasses = clsx([
     "w-full m-0 p-0 border-white outline-none my-auto",
     {
       "bg-gray-100": props.bgColor === "gray",
       "bg-white": props.bgColor === "white",
     },
   ]);
-  const labelStyle = clsx([
+  const labelClasses = clsx([
     {
       "text-black": props.textColor === "black",
       "text-gray-200": props.textColor === "glay",
@@ -56,16 +56,16 @@ export const InputText: VFC<TextareaType | InputType> = (props) => {
     <div className="flex flex-col">
       {props.label ? (
         <div>
-          <span className={labelStyle}>{props.label}</span>
+          <span className={labelClasses}>{props.label}</span>
         </div>
       ) : null}
-      <div className={divStyle}>
+      <div className={divClasses}>
         {/* 先頭にアイコンを表示する */}
         {props.startIcon ? props.startIcon : null}
         {/* 複数行表示と切り替える */}
         {isTextarea(props) ? (
           <textarea
-            className={inputStyle}
+            className={inputClasses}
             placeholder={props.placeholder}
             maxLength={props.maxLength}
             rows={props.rows}
@@ -75,7 +75,7 @@ export const InputText: VFC<TextareaType | InputType> = (props) => {
         ) : (
           <input
             type="text"
-            className={inputStyle}
+            className={inputClasses}
             placeholder={props.placeholder}
             value={props.value}
             onChange={props.onChange}
