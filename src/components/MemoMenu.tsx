0412bb80d490782952a02653f269fa86ab19863e
takasaki376 <75880059+takasaki376@users.noreply.github.com>
2021-04-16 11:46:50 +0900

    🩹 フラグメントを削除

--- src/components/MemoMenu.tsx
+++ src/components/MemoMenu.tsx
@@ -22,161 +22,159 @@ export const MemoMenu: VFC<Props> = (props) => {
   const cancelButtonRef = useRef() as MutableRefObject<HTMLElement | null>;
 
   return (
-    <>
-      <Transition show={props.menuOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          id="modal"
-          className="fixed inset-0 z-10 overflow-y-auto"
-          initialFocus={cancelButtonRef}
-          static
-          open={props.menuOpen}
-          onClose={props.onMenuClose}
-        >
-          <div className="min-h-screen px-4 text-center items-end">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-black opacity-20" />
-            </Transition.Child>
+    <Transition show={props.menuOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        id="modal"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        initialFocus={cancelButtonRef}
+        static
+        open={props.menuOpen}
+        onClose={props.onMenuClose}
+      >
+        <div className="min-h-screen px-4 text-center items-end">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-20" />
+          </Transition.Child>
 
-            <span className="block h-screen align-middle bottom-0" aria-hidden="true">
-              &#8203;
-            </span>
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <div className="inline-block w-full max-w-md px-6 pb-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-t-2xl">
-                <header className="flex">
-                  <div className="flex-auto" />
-                  <div>
-                    <Button
-                      button
-                      className="hover:bg-gray-100 rounded-full ml-auto"
-                      bgColor="gray"
-                      onClick={props.onMenuClose}
-                    >
-                      <XIcon />
-                    </Button>
-                  </div>
-                </header>
-                <main>
-                  {props.publicFlg ? (
-                    <Button
-                      button
-                      className="w-full"
-                      bgColor="gray"
-                      justifyContent="justify-between"
-                      group="top"
-                      endIcon={<EyeOffIcon />}
-                      onClick={props.onPublicClick}
-                    >
-                      <strong>
-                        <span className="my-auto text-blue-500">非公開にする</span>
-                      </strong>
-                    </Button>
-                  ) : (
-                    <Button
-                      button
-                      className="w-full"
-                      bgColor="gray"
-                      justifyContent="justify-between"
-                      group="top"
-                      endIcon={<EyeIcon />}
-                      onClick={props.onPublicClick}
-                    >
-                      <strong>
-                        <span className="my-auto text-blue-500">公開する</span>
-                      </strong>
-                    </Button>
-                  )}
+          <span className="block h-screen align-middle bottom-0" aria-hidden="true">
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full max-w-md px-6 pb-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-t-2xl">
+              <header className="flex">
+                <div className="flex-auto" />
+                <div>
                   <Button
                     button
-                    className="w-full"
+                    className="hover:bg-gray-100 rounded-full ml-auto"
                     bgColor="gray"
-                    justifyContent="justify-between"
-                    group="bottom"
-                    endIcon={<TrashIcon />}
-                    onClick={props.onDeleteModalOpen}
+                    onClick={props.onMenuClose}
                   >
-                    <strong>
-                      <span className="my-auto text-red-500">削除する</span>
-                    </strong>
+                    <XIcon />
                   </Button>
-                  {props.publicFlg ? null : (
-                    <p className="text-center m-2">
-                      <strong>以下は公開後に操作ができます</strong>
-                    </p>
-                  )}
+                </div>
+              </header>
+              <main>
+                {props.publicFlg ? (
                   <Button
                     button
                     className="w-full"
                     bgColor="gray"
                     justifyContent="justify-between"
                     group="top"
-                    disabled={!props.publicFlg}
-                    endIcon={<TwitterIcon disabled={!props.publicFlg} />}
+                    endIcon={<EyeOffIcon />}
+                    onClick={props.onPublicClick}
                   >
                     <strong>
-                      <span className="my-auto">Twitterでシェアする</span>
+                      <span className="my-auto text-blue-500">非公開にする</span>
                     </strong>
                   </Button>
+                ) : (
+                  <Button
+                    button
+                    className="w-full"
+                    bgColor="gray"
+                    justifyContent="justify-between"
+                    group="top"
+                    endIcon={<EyeIcon />}
+                    onClick={props.onPublicClick}
+                  >
+                    <strong>
+                      <span className="my-auto text-blue-500">公開する</span>
+                    </strong>
+                  </Button>
+                )}
+                <Button
+                  button
+                  className="w-full"
+                  bgColor="gray"
+                  justifyContent="justify-between"
+                  group="bottom"
+                  endIcon={<TrashIcon />}
+                  onClick={props.onDeleteModalOpen}
+                >
+                  <strong>
+                    <span className="my-auto text-red-500">削除する</span>
+                  </strong>
+                </Button>
+                {props.publicFlg ? null : (
+                  <p className="text-center m-2">
+                    <strong>以下は公開後に操作ができます</strong>
+                  </p>
+                )}
+                <Button
+                  button
+                  className="w-full"
+                  bgColor="gray"
+                  justifyContent="justify-between"
+                  group="top"
+                  disabled={!props.publicFlg}
+                  endIcon={<TwitterIcon disabled={!props.publicFlg} />}
+                >
+                  <strong>
+                    <span className="my-auto">Twitterでシェアする</span>
+                  </strong>
+                </Button>
 
-                  <Button
-                    button
-                    className="w-full"
-                    bgColor="gray"
-                    justifyContent="justify-between"
-                    group="bottom"
-                    disabled={!props.publicFlg}
-                    endIcon={<TwitterIcon disabled={!props.publicFlg} />}
-                  >
-                    <strong>
-                      <span className="my-auto">画像化してシェアする</span>
-                    </strong>
-                  </Button>
-                  <Button
-                    button
-                    className="w-full"
-                    bgColor="gray"
-                    justifyContent="justify-between"
-                    disabled={!props.publicFlg}
-                    endIcon={<ClipboardCopy disabled={!props.publicFlg} />}
-                  >
-                    <strong>
-                      <span className="my-auto">リンクをコピーする</span>
-                    </strong>
-                  </Button>
-                  <Button
-                    button
-                    className="w-full"
-                    bgColor="gray"
-                    justifyContent="justify-between"
-                    disabled={!props.publicFlg}
-                    endIcon={<DotsCircleHorizontalIcon disabled={!props.publicFlg} />}
-                  >
-                    <strong>
-                      <span className="my-auto">その他オプション</span>
-                    </strong>
-                  </Button>
-                </main>
-              </div>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition>
-    </>
+                <Button
+                  button
+                  className="w-full"
+                  bgColor="gray"
+                  justifyContent="justify-between"
+                  group="bottom"
+                  disabled={!props.publicFlg}
+                  endIcon={<TwitterIcon disabled={!props.publicFlg} />}
+                >
+                  <strong>
+                    <span className="my-auto">画像化してシェアする</span>
+                  </strong>
+                </Button>
+                <Button
+                  button
+                  className="w-full"
+                  bgColor="gray"
+                  justifyContent="justify-between"
+                  disabled={!props.publicFlg}
+                  endIcon={<ClipboardCopy disabled={!props.publicFlg} />}
+                >
+                  <strong>
+                    <span className="my-auto">リンクをコピーする</span>
+                  </strong>
+                </Button>
+                <Button
+                  button
+                  className="w-full"
+                  bgColor="gray"
+                  justifyContent="justify-between"
+                  disabled={!props.publicFlg}
+                  endIcon={<DotsCircleHorizontalIcon disabled={!props.publicFlg} />}
+                >
+                  <strong>
+                    <span className="my-auto">その他オプション</span>
+                  </strong>
+                </Button>
+              </main>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
