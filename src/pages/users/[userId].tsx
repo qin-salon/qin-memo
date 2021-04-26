e6e0b69d832f413cff81c24b7a85c2c4a7230111
skoto <0120koto@gmail.com>
2021-04-26 18:39:01 +0900

    ♻️ ユーザーページのCSSリファクタリング。

--- src/pages/users/[userId].tsx
+++ src/pages/users/[userId].tsx
@@ -1,9 +1,10 @@
 import { SearchIcon } from "@heroicons/react/outline";
 import type { NextPage } from "next";
+import Link from "next/link";
 import { Avatar } from "src/components/shared/Avatar";
-import { Button } from "src/components/shared/Button";
 import { Header } from "src/components/shared/Header";
 import { InputText } from "src/components/shared/InputText";
+import { WidthContainer } from "src/components/shared/WidthContainer";
 import { MemoCard } from "src/components/users/MemoCard";
 import { EXAMPLE_USER_01 } from "src/models/user";
 import type { ListNoteType } from "src/types/types";
@@ -18,33 +19,38 @@ const User: NextPage = () => {
   const { data: listNote, error } = useSWR<ListNoteType[]>(`/users/${user.id}/notes`);
 
   return (
-    <div className="flex overscroll-none flex-col h-screen">
+    <div>
       <Header />
+      <WidthContainer>
+        <div className="space-y-7">
+          <div className="flex items-center space-x-4">
+            <Avatar alt={user.name} src={user.avatarUrl} className="w-16 h-16" />
+            <div className="flex flex-col">
+              <span className="font-bold">{user.name}</span>
+              <Link href="/settings/profile">
+                <a className="text-sm font-bold text-blue-500 hover:underline">プロフィール設定</a>
+              </Link>
+            </div>
+          </div>
 
-      <div className="w-10/12 lg:w-1/2 mx-auto">
-        <div className="flex flex-row my-4">
-          <div className="flex flex-col">
-            <Avatar alt={user.name} src={user.avatarUrl} className="w-12 h-12" />
+          <div>
+            <InputText
+              startIcon={<SearchIcon className="my-auto mr-2 w-6 h-6 text-blue-500" />}
+              placeholder="メモを検索する"
+            />
           </div>
-          <div className="flex flex-col">
-            <span className="my-0 ml-2">{user.name}</span>
-            <Button linkProps={{ href: "/settings/profile" }} size="extrasmall" bgColor="transparent" textColor="blue">
-              プロフィール設定
-            </Button>
-          </div>
+
+          {error ? <div>メモが登録されていません</div> : null}
+
+          {listNote ? (
+            <div>
+              {listNote.map((note: ListNoteType) => {
+                return <MemoCard key={note.id} note={note} />;
+              })}
+            </div>
+          ) : null}
         </div>
-        <div className="block lg:hidden">
-          <InputText startIcon={<SearchIcon className="my-auto mr-2 w-6 h-6" />} placeholder="メモを検索する" />
-        </div>
-        {error ? <div>メモが登録されていません</div> : null}
-        {listNote ? (
-          <div className="flex flex-col w-full h-full">
-            {listNote.map((note: ListNoteType) => {
-              return <MemoCard key={note.id} note={note} />;
-            })}
-          </div>
-        ) : null}
-      </div>
+      </WidthContainer>
     </div>
   );
 };
