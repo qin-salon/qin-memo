3b1b39d8b6f36d309f5cbbd96c9c4d0339230cdd
skoto <0120koto@gmail.com>
2021-05-02 21:50:11 +0900

    🎨 アカウント連携ページのデザイン調整・リファクタ

--- src/pages/settings/qin/account.tsx
+++ src/pages/settings/qin/account.tsx
@@ -1,39 +1,28 @@
-import { ChevronLeftIcon } from "@heroicons/react/outline";
 import type { NextPage } from "next";
 import { AppleIcon } from "src/components/icon/AppleIcon";
 import { GoogleIcon } from "src/components/icon/GoogleIcon";
-import { QinAccountIcon } from "src/components/icon/QinAccountIcon";
-import { Button } from "src/components/shared/Button";
+import { Header } from "src/components/shared/Header";
 
 const SettingsAccount: NextPage = () => {
   return (
-    <div className="flex justify-center px-5 h-screen text-black bg-white">
-      <div className="w-80">
-        <div className="flex relative justify-center py-3 mb-6">
-          <Button button bgColor="transparent" className="absolute left-0 top-0 mx-0 my-0 px-0 py-3.5">
-            <ChevronLeftIcon className="w-5 h-5" />
-          </Button>
-          <QinAccountIcon className="w-28 sm:w-32" />
-        </div>
-        <h2 className="mb-10 text-2xl font-bold">アカウントの連携</h2>
-        <div className="flex justify-between items-center mb-3">
-          <div className="flex items-center">
+    <div className="pb-20">
+      <Header page="setting" left="back" />
+
+      <div className="px-4 mx-auto max-w-screen-sm">
+        <h1 className="text-xl font-bold">アカウントの連携</h1>
+
+        <ul className="mt-8 space-y-4">
+          <li className="flex justify-between items-center">
             <GoogleIcon />
-            <p className="ml-3 font-bold">Google</p>
-          </div>
-          <Button button bgColor="gray" className="my-0 mx-0 text-xs">
-            解除する
-          </Button>
-        </div>
-        <div className="flex justify-between items-center mb-3">
-          <div className="flex items-center">
-            <AppleIcon iconColor="black" />
-            <p className="ml-3 font-bold">Apple</p>
-          </div>
-          <Button button bgColor="blue" className="my-0 mx-0 text-xs">
-            連携する
-          </Button>
-        </div>
+            <div className="ml-3 flex-1 font-bold">Google</div>
+            <button className="py-2 px-4 text-sm font-bold bg-gray-100 rounded-full">解除する</button>
+          </li>
+          <li className="flex justify-between items-center">
+            <AppleIcon />
+            <div className="ml-3 flex-1 font-bold">Apple</div>
+            <button className="py-2 px-4 text-sm font-bold bg-blue-500 text-white rounded-full">連携する</button>
+          </li>
+        </ul>
       </div>
     </div>
   );
