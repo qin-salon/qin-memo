cebe48f9b397e08354f6257d4fde9295c6aa1717
skoto <0120koto@gmail.com>
2021-05-02 16:55:23 +0900

    🌐 next.config.jsを使ったlangの設定、それに伴い_documentが不要になったので削除

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  future: { strictPostcssConfiguration: true },
+  i18n: { locales: ["ja"], defaultLocale: "ja" },
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
