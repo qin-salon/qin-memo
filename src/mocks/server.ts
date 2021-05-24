8d9f2ef066ea74824a2e23ef8edafd91d20a9954
skoto <0120koto@gmail.com>
2021-05-24 13:39:50 +0900

    ⬆️ Upgrade msw.

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,5 +1,4 @@
 import { setupServer } from "msw/node";
 import { handlers } from "src/mocks/handlers";
 
-// Setup requests interception using the given handlers.
 export const server = setupServer(...handlers);
