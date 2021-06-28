cc62ff324ca8b828ed60b6fdc058723c96cee674
skoto <0120koto@gmail.com>
2021-06-28 17:28:20 +0900

    🚑 Lintが非常に遅くなるので一時しのぎ解決

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ module.exports = {
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
     "tailwindcss/classnames-order": "warn",
-    "tailwindcss/no-custom-classname": "warn",
+    // "tailwindcss/no-custom-classname": "warn",
     "tailwindcss/no-contradicting-classname": "error",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-var-requires": "off",
