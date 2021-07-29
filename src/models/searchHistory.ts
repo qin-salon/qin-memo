4e0c09b9ec9ccd3b1eaa3ec9471fc659b564cdaf
skoto <0120koto@gmail.com>
2021-07-29 19:45:38 +0900

    🏷️ 型エラー修正

--- src/models/searchHistory.ts
+++ src/models/searchHistory.ts
@@ -1,7 +1,7 @@
 import type { SearchHistoryType } from "src/types/types";
 
 export const EXAMPLE_SEARCH_HISTORIES: SearchHistoryType[] = [
-  { id: 3, keyword: "Vue.js" },
-  { id: 2, keyword: "React" },
-  { id: 1, keyword: "JavaScript" },
+  { id: 3, keyword: "Vue.js", createdOn: "2021-01-01T00:00:00.000Z" },
+  { id: 2, keyword: "React", createdOn: "2021-02-01T00:00:00.000Z" },
+  { id: 1, keyword: "JavaScript", createdOn: "2021-03-01T00:00:00.000Z" },
 ];
