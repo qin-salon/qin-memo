767b6685fc5a34dc4092f29df95b4bcc9dbe90a9
skoto <0120koto@gmail.com>
2021-07-31 15:33:48 +0900

    🩹 微修正

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -20,7 +20,7 @@ export const NoteList: VFC<NoteListProps> = (props) => {
               className="py-4 px-4 sm:px-6 w-full bg-gray-100 dark:bg-gray-700 rounded-xl shadow animate-pulse"
             >
               <div className="w-3/4 h-3.5 sm:h-4 bg-gray-200 dark:bg-gray-600 rounded"></div>
-              <div className="mt-2.5 h-3 sm:h-3.5 bg-gray-200 dark:bg-gray-600 rounded"></div>
+              <div className="mt-2.5 h-3.5 bg-gray-200 dark:bg-gray-600 rounded"></div>
               <div className="mt-6 w-16 h-3.5 bg-gray-200 dark:bg-gray-600 rounded"></div>
             </li>
           );
