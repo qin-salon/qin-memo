3861a1b98f0c3ebd746adb7858aad37176b61940
skoto <0120koto@gmail.com>
2021-08-25 16:52:39 +0900

    🐛 Viewerの改行や空白の考慮

--- src/pages/memo/NoteViewer.tsx
+++ src/pages/memo/NoteViewer.tsx
@@ -1,5 +1,5 @@
 import type { NoteWithUserType } from "src/api/handler/note/type";
 
 export const NoteViewer = (props: NoteWithUserType) => {
-  return <p className="text-lg leading-loose">{props.content}</p>;
+  return <p className="text-lg leading-loose whitespace-pre-wrap">{props.content}</p>;
 };
