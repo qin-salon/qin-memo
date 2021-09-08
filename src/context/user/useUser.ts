f9fe5658c282fad695f433b883f54448f4fc7ba8
skoto <0120koto@gmail.com>
2021-09-08 19:54:51 +0900

    🎨 ユーザー取得周りを整理

--- src/context/user/useUser.ts
+++ src/context/user/useUser.ts
@@ -1,48 +1,8 @@
-import type { AuthUserContext } from "next-firebase-auth";
 import { useAuthUser } from "next-firebase-auth";
 import { API_URL } from "src/api/endpoint";
-import { isUserType } from "src/api/handler/user/type";
 import useSWRImmutable from "swr/immutable";
 
-const fetchUser = async (key: string, authUser: AuthUserContext) => {
-  const idToken = await authUser.getIdToken();
-  const res = await fetch(key, idToken ? { headers: { authorization: `Bearer ${idToken}` } } : undefined);
-
-  if (res.status === 404) {
-    const res = await fetch(`${API_URL}/users`, {
-      method: "POST",
-      headers: { authorization: `Bearer ${idToken}`, "content-type": "application/json" },
-      body: JSON.stringify({
-        uid: authUser.id,
-        accountName: authUser.displayName,
-        avatarUrl: authUser.photoURL,
-      }),
-    });
-    const data = await res.json();
-
-    if (!isUserType(data)) {
-      throw new Error("Failed to create user");
-    }
-
-    if (!data.enabledQinMemo) {
-      await fetch(`${API_URL}/users/${data?.id}`, {
-        method: "PUT",
-        headers: { authorization: `Bearer ${idToken}`, "content-type": "application/json" },
-        body: JSON.stringify({ enabledQinMemo: true }),
-      });
-    }
-
-    return data;
-  }
-
-  const data = await res.json();
-
-  if (!isUserType(data)) {
-    throw new Error("Failed to create user");
-  }
-
-  return data;
-};
+import { getUser } from "./getUser";
 
 /**
  * @package
@@ -55,7 +15,7 @@ export const useUser = () => {
     ...rest
   } = useSWRImmutable(authUser.id ? `${API_URL}/users` : null, {
     fetcher: async (key) => {
-      const user = await fetchUser(key, authUser);
+      const user = await getUser(key, authUser);
       return user;
     },
   });
