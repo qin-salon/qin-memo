8f840ca90e38d2651dfe53c64c4b2548af37575a
skoto <0120koto@gmail.com>
2021-09-11 21:13:59 +0900

    ✨ react-hook-formを導入し、フォームにバリデーションを追加

--- src/pages/setting/qin/user/ProfileForm.tsx
+++ src/pages/setting/qin/user/ProfileForm.tsx
@@ -1,29 +1,30 @@
 import type { VFC } from "react";
+import { useForm } from "react-hook-form";
 import { Avatar } from "src/component/Avatar";
 import { Button } from "src/component/Button";
 import { Input } from "src/component/Form";
 import { useUser } from "src/util/user";
 
-import { useProfileForm } from "./useProfileForm";
+import { useFile } from "./useFile";
+import { useUpdateUser } from "./useUpdateUser";
+
+export type UserUpdateForm = { accountName: string; userName: string };
+
+type ProfileFormProps = { accountName?: string; userName?: string };
 
 /**
  * @package
  */
-export const ProfileForm: VFC = () => {
+export const ProfileForm: VFC<ProfileFormProps> = () => {
   const { user } = useUser();
-  const {
-    isLoading,
-    imageUrl,
-    userNameRef,
-    accountNameRef,
-    imageRef,
-    handleChangeFile,
-    handleSave,
-    handleOpenFileDialog,
-  } = useProfileForm();
+  const { selectedFile, imageUrl, imageRef, handleChangeFile, handleOpenFileDialog } = useFile();
+  const { isUpdating, updateUser } = useUpdateUser(selectedFile);
+  const { register, handleSubmit, formState } = useForm<UserUpdateForm>({
+    defaultValues: { accountName: user?.accountName ?? "", userName: user?.userName ?? "" },
+  });
 
   return (
-    <div>
+    <form onSubmit={handleSubmit(updateUser)}>
       <div className="space-y-6 sm:space-y-8">
         <div>
           <div className="flex justify-start items-center space-x-6">
@@ -47,18 +48,38 @@ export const ProfileForm: VFC = () => {
             </Button>
           </div>
         </div>
-        <Input name="accountName" label="名前" defaultValue={user?.accountName} ref={accountNameRef} />
-        <Input name="userName" label="ユーザー名" prefix="@" defaultValue={user?.userName} ref={userNameRef} />
+
+        <Input
+          label="名前"
+          {...register("accountName", {
+            required: { value: true, message: "入力必須です" },
+            maxLength: { value: 64, message: "64文字以下にする必要があります" },
+            minLength: { value: 4, message: "4文字以上にする必要があります" },
+          })}
+          error={formState.errors.accountName?.message}
+        />
+
+        <Input
+          label="ユーザー名"
+          prefix="@"
+          {...register("userName", {
+            required: { value: true, message: "入力必須です" },
+            maxLength: { value: 16, message: "16文字以下にする必要があります" },
+            minLength: { value: 4, message: "4文字以上にする必要があります" },
+            pattern: { value: /^[A-Za-z0-9_]+$/i, message: "半角英数字だけご使用できます" },
+          })}
+          error={formState.errors.userName?.message}
+        />
       </div>
 
       <div className="mt-12 space-y-4">
         {user ? (
-          <Button variant="solid-blue" className="p-3 w-full" onClick={handleSave} disabled={isLoading}>
+          <Button type="submit" variant="solid-blue" className="p-3 w-full" disabled={isUpdating}>
             保存する
           </Button>
         ) : (
           <>
-            <Button variant="solid-blue" className="p-3 w-full" disabled={isLoading}>
+            <Button type="submit" variant="solid-blue" className="p-3 w-full" disabled={isUpdating}>
               登録してはじめる
             </Button>
             <Button variant="solid-gray" className="p-3 w-full">
@@ -67,6 +88,6 @@ export const ProfileForm: VFC = () => {
           </>
         )}
       </div>
-    </div>
+    </form>
   );
 };
