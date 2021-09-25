4ae3b4ac8f78760adec949883b4827a94ed05ec3
abc123931 <abc123931@gmail.com>
2021-09-25 23:39:47 +0900

    🔊 ログ追加

--- src/pages/api/android-auth.page.ts
+++ src/pages/api/android-auth.page.ts
@@ -3,7 +3,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const insertQuotaMember = async (req: NextApiRequest, res: NextApiResponse) => {
   const redirect = `intent://callback?${new URLSearchParams(
     req.body
-  ).toString()}#Intent;package=${"sh.memo.qin.dev"};scheme=signinwithapple;end`;
+  ).toString()}#Intent;package=sh.memo.qin.dev;scheme=signinwithapple;end`;
+
+  console.info(req.body);
+  console.info(redirect);
   res.redirect(307, redirect);
 };
 
