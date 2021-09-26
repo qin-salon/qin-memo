475cfb87a7ee2f9337e4d46e5d32f9f3513e7390
abc123931 <abc123931@gmail.com>
2021-09-26 10:28:27 +0900

    🐛 package名の修正

--- src/pages/api/android-auth.page.ts
+++ src/pages/api/android-auth.page.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const insertQuotaMember = async (req: NextApiRequest, res: NextApiResponse) => {
   const redirect = `intent://callback?${new URLSearchParams(
     req.body
-  ).toString()}#Intent;package=sh.memo.qin.dev;scheme=signinwithapple;end`;
+  ).toString()}#Intent;package=${"sh.qin.memo.dev"};scheme=signinwithapple;end`;
 
   console.info(req.body);
   console.info(redirect);
