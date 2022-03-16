f2d7c560d85159ea89f3d94a9e6589e1ef87d193
skoto <0120koto@gmail.com>
2022-03-16 14:29:06 +0900

    ✅ Next.jsのSWCを使ったテスト設定に変更

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
-module.exports = {
-  roots: ["<rootDir>"],
-  moduleFileExtensions: ["js", "ts", "tsx", "json"],
-  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
-  watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
-  transform: { "^.+\\.(ts|tsx)$": "babel-jest" },
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
-  moduleNameMapper: {
-    "^.+\\.module\\.(css|scss)$": "identity-obj-proxy",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/test/__mocks__/fileMock.js",
-    "src/(.*)": "<rootDir>/src/$1",
-    "test/(.*)": "<rootDir>/test/$1",
-  },
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ **/
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
 };
+
+module.exports = createJestConfig(customJestConfig);
